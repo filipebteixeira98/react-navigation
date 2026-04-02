0c2cfa7c6d44b3916460aaf8cf1bc4cb35002cb7
Filipe Teixeira <filipebarrosteixeira98@gmail.com>
2026-04-01 22:01:09 -0300

    feat: implement navigation handlers for Home and Product screens

--- src/app/Home.tsx
+++ src/app/Home.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 
 import { ButtonIcon } from "@/components/ButtonIcon";
@@ -5,11 +6,16 @@ import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 
 export function Home() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
         <Title>Home</Title>
-        <ButtonIcon name="add-circle" />
+        <ButtonIcon
+          name="add-circle"
+          onPress={() => navigation.navigate("product")}
+        />
       </Header>
     </View>
   );
--- src/app/Product.tsx
+++ src/app/Product.tsx
@@ -1,3 +1,4 @@
+import { useNavigation } from "@react-navigation/native";
 import { View } from "react-native";
 
 import { ButtonIcon } from "@/components/ButtonIcon";
@@ -5,10 +6,15 @@ import { Header } from "@/components/Header";
 import { Title } from "@/components/Title";
 
 export function Product() {
+  const navigation = useNavigation();
+
   return (
     <View style={{ flex: 1, padding: 32, paddingTop: 54 }}>
       <Header>
-        <ButtonIcon name="arrow-circle-left" />
+        <ButtonIcon
+          name="arrow-circle-left"
+          onPress={() => navigation.goBack()}
+        />
         <Title>Product</Title>
       </Header>
     </View>
